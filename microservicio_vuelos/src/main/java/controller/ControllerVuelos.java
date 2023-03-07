7cffc2d3c3c5fb30c2b8160e7dc70dab10d9f415
adrian <adrianrodriguez0510@gmail.com>
2023-03-07 18:37:15 -0300

    Cliente Web

--- microservicio_vuelos/src/main/java/controller/ControllerVuelos.java
+++ microservicio_vuelos/src/main/java/controller/ControllerVuelos.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.client.RestTemplate;
 import model.Vuelo;
 import service.IServiceVuelos;
 
+@CrossOrigin(origins="*")
 @RestController
 public class ControllerVuelos {
 	
